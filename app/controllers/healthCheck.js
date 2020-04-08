234860128c1de4624ae17f2d16b774d5333810df
raparicio6 <rodrigoaparicio6@gmail.com>
2020-04-08 03:26:21 -0300

    moving healtCheck controller test to other directory

--- app/controllers/healthCheck.js
+++ app/controllers/healthCheck.js
@@ -1,7 +1,7 @@
-const redisClient = require('../services/redis');
+const { redisClient } = require('../services/redis');
 const {
   EXPIRE,
-  REDIS_HEALTH_KEY_EXPIRE_TIME,
+  REDIS_HEALTH_EXPIRE_TIME,
   REDIS_HEALTH_KEY,
   REDIS_HEALTH_DEFAULT_VALUE
 } = require('../constants');
@@ -10,6 +10,6 @@ exports.healthCheck = (_, res) => res.send({ uptime: process.uptime() });
 
 exports.redisHealthCheck = (_, res) =>
   redisClient
-    .set(REDIS_HEALTH_KEY, REDIS_HEALTH_DEFAULT_VALUE, EXPIRE, REDIS_HEALTH_KEY_EXPIRE_TIME)
+    .set(REDIS_HEALTH_KEY, REDIS_HEALTH_DEFAULT_VALUE, EXPIRE, REDIS_HEALTH_EXPIRE_TIME)
     .then(() => redisClient.get(REDIS_HEALTH_KEY))
     .then(result => res.send({ result }));
